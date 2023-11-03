5382026e54b3ea31742db8077d3b717815e7c6bf
roeldegoede <roeldegoede@outlook.com>
2023-11-03 13:26:06 +0100

    added apis of NDBC and NOAA

--- cht_observations/_noaa_coops.py
+++ cht_observations/_noaa_coops.py
@@ -1,20 +1,64 @@
 from cht_observations.observation_stations import StationSource
-from noaa_coops import Station
+from noaa_coops import Station, get_stations_from_bbox
 
+import requests
 
 class Source(StationSource):
     def __init__(self):
         pass
 
-    def list_stations(self):
-        pass
+    def get_active_stations(self):
+        data_url = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json"
+        response = requests.get(data_url)
+        json_dict = response.json()
+
+        station_list = []
+        for station_dict in json_dict["stations"]:
+            station_list.append({"name": station_dict["name"],
+                                    "id": station_dict["id"],
+                                    "lon": float(station_dict["lng"]),
+                                    "lat": float(station_dict["lat"])})
+
+        self.active_stations = station_list    
+        return station_list
 
     def get_meta_data(self, id):
-        pass
+        station = Station(id=id)
+        meta_data = station.metadata
+        return meta_data
 
     def get_data(
-        self, station_id, tstart, tstop, varname="water_level", units="SI", datum="MSL"
+        self,
+        id:int, 
+        tstart, 
+        tstop, 
+        varname:str ="water_level", 
+        units:str="SI", 
+        datum:str="MSL"
     ):
+        """"
+        Get data from NOAA COOPS
+
+        Parameters
+        ----------
+        id : str
+            Station ID
+        tstart : datetime
+            Start time
+        tstop : datetime
+            Stop time
+        varname : str
+            Variable name
+        units : str
+            Units
+        datum : str
+            Datum, e.g. MSL or NAVD
+
+        Returns
+        -------
+        df : pandas.DataFrame
+            Data frame with data
+        """"
         t0_string = tstart.strftime("%Y%m%d")
         t1_string = tstop.strftime("%Y%m%d")
 
@@ -24,7 +68,7 @@ class Source(StationSource):
         if units == "SI":
             units = "metric"
 
-        station = Station(id=station_id)
+        station = Station(id=id)
         df = station.get_data(
             begin_date=t0_string,
             end_date=t1_string,
---
+++ examples/example_noaa_coops.py
@@ -0,0 +1,11 @@
+"""
+Example: NOAA COOPS
+"""	
+
+import datetime
+import cht_observations.observation_stations as obs
+
+coops = obs.source("noaa_coops")
+t0 = datetime.datetime(2015, 1, 1)
+t1 = datetime.datetime(2015, 1, 10)
+df = coops.get_data("9447130", t0, t1)
